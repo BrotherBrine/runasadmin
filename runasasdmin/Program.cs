6dd4ec445c26ebed62ad1909bfc3d73745a7a93a
Zachary <zacharytstevenson@gmail.com>
2019-10-02 10:51:31 -0400

    initial commit

---
+++ runasasdmin/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace runasadmin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string arguments = "";
+                if (args.Length == 0)
+                {
+                    return;
+                }
+                else
+                {
+                    foreach (string arg in args)
+                    {
+                        arguments += $"{arg} ";
+                    }
+                }
+                var process = new Process();
+                var startinfo = new ProcessStartInfo($@"{arguments}");
+                startinfo.UseShellExecute = true;
+                startinfo.CreateNoWindow = true;
+                process.StartInfo = startinfo;
+                process.Start();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+            }
+        }
+    }
+}
